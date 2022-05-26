39b2e2c73b92099ccd09d566f89f5ccb8ca78ee1
Matěj Vopálka <xvopal03@vutbr.cz>
2022-05-26 22:22:42 +0200

    Swagger disablet

--- Firios/Program.cs
+++ Firios/Program.cs
@@ -22,8 +22,8 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
+//app.UseSwagger();
+//app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
